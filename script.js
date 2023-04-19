2e1c5697b406cbc4030059d5d35081f5024ce793
Kevin Kipkemboi <4940465+kevinkip@users.noreply.github.com>
2023-04-19 15:24:58 -0700

    each player can choose the character and change name

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ const characters = document.querySelectorAll('.character');
 const characterCall = document.querySelector('#character-call');
 const titleSound = document.querySelector('#character-select-music');
 const body = document.body;
+console.log(body);
 const gameContainer = document.querySelector('.game-container');
 const titleScreen = document.querySelector('.titleScreen');
 const chooseBattlefield = document.querySelector('.choose-battlefield');
 const loadingScreen = document.querySelector('.loading-screen');
 const gameScreen = document.querySelector('.game-screen');
-const playerOneName = document.querySelector('.player1');
-const playerTwoName = document.querySelector('.player2');
+let playerOneName = document.querySelector('.player1');
+let playerTwoName = document.querySelector('.player2');
 const playerOnePic = document.querySelector('.p1');
 const playerTwoPic = document.querySelector('.p2');
-
-
 
 console.log(characters);
 
@@ -40,89 +39,124 @@ window.addEventListener('load', () => {
     //turn off screens 1,2,4,5
 })
 
-playerOneName.addEventListener('click', () => { 
-    body.style.cursor = 'url("/images/cursor/player1.png"), auto';
-    playerOneName.value = ""; 
-    playerOneName.addEventListener('keydown', (e) => {
-        if(e.keyCode == 13){
-            const newName = playerOneName.value;
-            console.log(newName);
-            playerOneName.value = newName;
+const characterScreen = (() => {
+    let playerNum = 1;
+    let currentPlayer;
+    let currentName;
+
+    const higherVol = () => {
+        characterCall.volume = 0.07;
+    }
+    
+    const midVol = () => {
+        characterCall.volume = 0.05;
+    }
+    
+    const lowerVol = () => {
+        characterCall.volume = 0.019;
+    }
+    
+    const capFirstLet = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const characterSelection = (name) => {
+        toggleChar(name, playerNum);
+    }
+
+    const toggleChar = (name, char) => {
+        if(char == 1){
+            player1(name);
+        } else if (char == 2){
+            player2(name);
         }
-    })
-    playerOneName.addEventListener('keyup', (e) => {
-        if (e.keyCode === 13) {
-        e.target.blur();
+        return {
+            currentPlayer, currentName
         }
     }
-    )
-})
 
-playerTwoName.addEventListener('click', () => {
-    body.style.cursor = 'url("/images/cursor/player2.png"), auto';
-    playerTwoName.value = "BOT";
-    playerTwoName.addEventListener('keydown', (e) => {
-        if(e.keyCode == 13){
-            const newName = playerTwoName.value;
-            console.log(newName);
-            playerTwoName.value = newName;
-        }
-    })
-    playerTwoName.addEventListener('keyup', (e) => {
-        if (e.keyCode === 13) {
-        e.target.blur();
-        }
+    const player1 = (name ) => {
+        playerOnePic.setAttribute('src', `/images/full-size-character/${name}.png`);
+        playerOneName.value = capFirstLet(name);
+        console.log(playerOneName);
     }
-    )
-})
 
-const characterSelection = (item) => {
-    characterCall.setAttribute('src', `/sound/${item}.wav`);
-    fullCharPic.setAttribute('src', `/images/full-size-character/${item}.png`);
-    playerOneName.value = capFirstLet(item);
-    console.log(characterCall.volume);
-    characterCall.play();
-}
+    const player2 = (name) => {
+        playerTwoPic.setAttribute('src', `/images/full-size-character/${name}.png`);
+        playerTwoName.value = capFirstLet(name);
+        console.log(playerTwoName);
+    }
 
-const higherVol = () => {
-    characterCall.volume = 0.07;
-}
+    const imagePlacement = (item) => {
+        imagePlacement(item);
+        characterCall.setAttribute('src', `/sound/${item}.wav`);
+        console.log(characterCall.volume);
+        characterCall.play();
+    }
 
-const midVol = () => {
-    characterCall.volume = 0.05;
-}
-
-const lowerVol = () => {
-    characterCall.volume = 0.019;
-}
-
-const capFirstLet = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-characters.forEach(item => {
-    item.addEventListener('click', () => {
-        console.log(item.id);
-        const name = item.id;
-        switch(name){
-            case('bowser'):
-                higherVol();
-                characterSelection(name);
-                break;
-            case('saitama'):
-                midVol();
-                characterSelection(name);
-                break;
-            case('jotaro'):
-                lowerVol();
-                characterSelection(name);
-                break;
-            default:
-                lowerVol();
-                characterSelection(name);
+    playerOneName.addEventListener('click', () => { 
+        playerNum = 1;
+        body.style.cursor = '/images/cursors/player1.png';
+        playerOneName.value = ""; 
+        playerOneName.addEventListener('keydown', (e) => {
+            if(e.keyCode == 13){
+                const newName = playerOneName.value;
+                console.log(newName);
+                playerOneName.value = newName;
+            }
+        })
+        playerOneName.addEventListener('keyup', (e) => {
+            if (e.keyCode === 13) {
+            e.target.blur();
+            }
         }
+        )
     })
-})
+    
+    playerTwoName.addEventListener('click', () => {
+        playerNum = 2;
+        body.style.cursor = '/images/cursors/player2.png';
+        playerTwoName.value = "BOT";
+        playerTwoName.addEventListener('keydown', (e) => {
+            if(e.keyCode == 13){
+                const newName = playerTwoName.value;
+                console.log(newName);
+                playerTwoName.value = newName;
+            }
+        })
+        playerTwoName.addEventListener('keyup', (e) => {
+            if (e.keyCode === 13) {
+            e.target.blur();
+            }
+        }
+        )
+    })
+        characters.forEach(item => {
+        item.addEventListener('click', () => {
+            const name = item.id;
+                switch(name){
+                    case('bowser'):
+                        higherVol();
+                        characterSelection(name);
+                        break;
+                    case('saitama'):
+                        midVol();
+                        characterSelection(name);
+                        break;
+                    case('jotaro'):
+                        lowerVol();
+                        characterSelection(name);
+                        break;
+                    default:
+                        lowerVol();
+                        characterSelection(name);
+                        }
+                })
+            }
+        )    
+})();
+
+
 // mario.addEventListener('click', () => {
 //     firstPlayer.setAttribute('src','/images/MarioSSB.png');
 //     console.log("the image has been changed");
